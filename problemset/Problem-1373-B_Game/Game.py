9c665ef8830c483fe3e54086124fdfdc4d4af7af
kayloportal <pinobaxp@gmail.com>
2025-11-09 22:46:35 +0330

    Logical fix

--- problemset/Problem-1373-B_Game/Game.py
+++ problemset/Problem-1373-B_Game/Game.py
@@ -3,8 +3,8 @@ n = int(input())
 def func(string):
   length = len(string)
   if length % 4 == 0 or length % 4 == 1:
-    return "DA"
-  return "NET"
+    return "NET"
+  return "DA"
 
 for i in range(n):
   print(func(input()))
