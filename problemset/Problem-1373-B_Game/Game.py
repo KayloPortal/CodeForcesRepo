8c27d1e0e57bee739169811b1ea75b24d15424c2
kayloportal <pinobaxp@gmail.com>
2025-11-09 23:08:43 +0330

    fixed Game

--- problemset/Problem-1373-B_Game/Game.py
+++ problemset/Problem-1373-B_Game/Game.py
@@ -1,10 +1,23 @@
 n = int(input())
 
 def func(string):
-  length = len(string)
-  if length % 4 == 0 or length % 4 == 1:
+  isAliceTurn = True
+  while(len(string) > 1):
+    for i in range(len(string) - 1):
+      if string[i] != string[i+1]:
+        isAliceTurn = not isAliceTurn
+        if len(string) == 2:
+          string = ""
+          break
+        string = string[0:i] + string[i+2:]
+        break
+    else:
+      break
+      
+  if isAliceTurn:
     return "NET"
-  return "DA"
+  else:
+    return "DA"
 
 for i in range(n):
   print(func(input()))
