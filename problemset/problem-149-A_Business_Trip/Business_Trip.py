557143281b168914308576c2614ac79376a7e1e7
kayloportal <pinobaxp@gmail.com>
2025-11-09 23:22:13 +0330

    p149a considered k = 0 test case

--- problemset/problem-149-A_Business_Trip/Business_Trip.py
+++ problemset/problem-149-A_Business_Trip/Business_Trip.py
@@ -1,11 +1,14 @@
 k = int(input())
-nums = list(map(int, input().split()))
-nums.sort(reverse=True)
-count = 0
-my_sum = 0
-for num in nums:
-  my_sum += num
-  count += 1
-  if my_sum >= k:
-    break
-print(count)
+if k == 0:
+  print(0)
+else:
+  nums = list(map(int, input().split()))
+  nums.sort(reverse=True)
+  count = 0
+  my_sum = 0
+  for num in nums:
+    my_sum += num
+    count += 1
+    if my_sum >= k:
+      break
+  print(count)
