36139c462ac70845ba040bf999808ba1e2c7c32b
kayloportal <pinobaxp@gmail.com>
2025-11-09 22:44:52 +0330

    Removed wrong function type + logical fix on line 5

--- problemset/Problem-1373-B_Game/Game.py
+++ problemset/Problem-1373-B_Game/Game.py
@@ -1,8 +1,8 @@
 n = int(input())
 
-def func(string) -> bool:
+def func(string):
   length = len(string)
-  if length % 4 != 0 or length % 4 == 1:
+  if length % 4 == 0 or length % 4 == 1:
     return "DA"
   return "NET"
 
